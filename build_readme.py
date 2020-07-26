94149a275aa74047ebecf67e55ace05d2ab032c3
philovdy <68233605+philovdy@users.noreply.github.com>
2020-07-26 12:34:29 +0530

    Remove date from blog title

--- build_readme.py
+++ build_readme.py
@@ -68,7 +68,7 @@ def fetch_blog_entries():
         {
             "title": entry["title"],
             "url": entry["link"].split("#")[0],
-            "published": entry["published"].split("T")[0],
+            # "published": entry["published"].split("T")[0],
         }
         for entry in entries
     ]
